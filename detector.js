bca12e3f3e87c752df6781b8681849e2c78fd176
Suan-Aik Yeo <syeo@enovafinancial.com>
2011-11-30 03:13:24 -0600

    Works in many more sites
    github:FI, wikimedia, wikipedia, Google Maps, acunote and more.
    Still does not get called in GMail for some reason

--- detector.js
+++ detector.js
@@ -1,3 +1,9 @@
 $(document).bind('keyup', 'ctrl+shift+f', function(){
-  $(':text').first().focus();
+  var $search_box;
+  var $search_inputs = $('input[type="search"]:visible');
+
+  if ($search_inputs.length) { $search_box = $search_inputs.first(); }
+  else { $search_box = $(':text:visible').first(); }
+
+  $search_box.focus();
 });
