d4afe72787605b19ae71830dc6ce201ed5c681d6
Suan-Aik Yeo <yeosuanaik@gmail.com>
2012-05-03 00:52:21 -0500

    use fancy settings page

---
+++ fancy-settings/source/manifest.js
@@ -0,0 +1,26 @@
+this.manifest = {
+    "name": "Search Key",
+    "icon": "../../icon_small.png",
+    "settings": [
+        {
+            "tab": "Search Key",
+            "group": "Settings",
+            "type": "text",
+            "label": "Keyboard Shortcut",
+            "text": 'badodz' 
+        },
+        {
+            "tab": "Search Key",
+            "group": "Help",
+            "type": "description",
+            "text": 'Found a problem or want to suggest a feature? Please let me know on the <a href="https://google.com" target="_blank">project\'s issues page</a>, or <a href="mailto:yeosuanaik@gmail.com">email me</a> instead (if you don\'t wanna set up a Github account).'
+        },
+        {
+            "tab": "Search Key",
+            "type": "description",
+            "label": "Donate",
+            "text": 'Do you find <em>Search Key</em> useful? If so, please consider leaving me a tip or spreading the word!<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://google.com" data-text="Search Key - Instantly go to any website\'s search box! (Chrome extension)">Tweet</a>'
+            // <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>' 
+        }
+    ]
+};
